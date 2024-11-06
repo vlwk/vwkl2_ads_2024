f34fdf885807c371fbafa7582427d8011ef3bfdb
Victor <victorlohwk@gmail.com>
2024-11-06 12:03:51 +0000

    Added download_price_paid_data to access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -17,3 +17,18 @@ def data():
 
 def hello_world():
   print("Hello from the data science library!")
+
+def download_price_paid_data(year_from, year_to):
+    # Base URL where the dataset is stored 
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    """Download UK house price data for given year range"""
+    # File name with placeholders
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
