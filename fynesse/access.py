9c6f9118b8a2bed883204cfcb5abba28a5b7dfd9
Victor <victorlohwk@gmail.com>
2024-11-07 00:10:54 +0000

    Added housing_upload_join_data to access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,6 +1,7 @@
 from .config import *
 import requests
 import pymysql
+import time
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -58,3 +59,23 @@ def create_connection(user, password, host, database, port=3306):
     except Exception as e:
         print(f"Error connecting to the MariaDB Server: {e}")
     return conn
+
+def housing_upload_join_data(conn, year):
+  start_date = str(year) + "-01-01"
+  end_date = str(year) + "-12-31"
+
+  cur = conn.cursor()
+  print('Selecting data for year: ' + str(year))
+  cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' + start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
+  rows = cur.fetchall()
+
+  csv_file_path = 'output_file.csv'
+
+  # Write the rows to the CSV file
+  with open(csv_file_path, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    # Write the data rows
+    csv_writer.writerows(rows)
+  print('Storing data for year: ' + str(year))
+  cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path + "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
+  print('Data stored for year: ' + str(year))
